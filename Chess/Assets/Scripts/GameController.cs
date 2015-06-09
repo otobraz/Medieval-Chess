1708655c40841e1d5f99600f2fa3b89b6f5c3f22
Oto Braz Assuncao <oto.braz@outlook.com>
2015-06-08 22:14:19 -0500

    Board is aware of the pieces movements.

--- Chess/Assets/Scripts/GameController.cs
+++ Chess/Assets/Scripts/GameController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	private string[,] gameBoard;
+	private int[,] gameBoard;
 
 	public GameObject blackPawn, blackRook, blackHorse, blackBishop, blackQueen, blackKing;
 
@@ -22,14 +22,16 @@ public class GameController : MonoBehaviour {
 	}
 
 	public void initializeGameBoard(){
-		gameBoard = new string[8, 8];
+		gameBoard = new int[8, 8];
 		for (int i = 0; i < 8; i++) {
-			gameBoard[1,i] = "wP";	Instantiate (whitePawn, new Vector3 (1, whitePawn.transform.position.y, i), Quaternion.identity);
-			gameBoard[6,i] = "bP";	Instantiate (blackPawn, new Vector3 (6, whitePawn.transform.position.y, i), Quaternion.identity);
+			gameBoard[1,i] = 1;	Instantiate (whitePawn, new Vector3 (1, whitePawn.transform.position.y, i), Quaternion.identity);
+			gameBoard[6,i] = 2;	Instantiate (blackPawn, new Vector3 (6, whitePawn.transform.position.y, i), Quaternion.identity);
+			gameBoard[0,i] = 1;
+			gameBoard[7,i] = 2;
 		}
 
-		gameBoard [0, 0] = "wR";	Instantiate (whiteRook, new Vector3 (0, whiteRook.transform.position.y, 0), Quaternion.identity);
-		gameBoard [0, 0] = "wH";	Instantiate (whiteHorse, new Vector3 (0, whiteHorse.transform.position.y, 1), Quaternion.identity);
+		/*gameBoard [0, 0] = "wR";	Instantiate (whiteRook, new Vector3 (0, whiteRook.transform.position.y, 0), Quaternion.identity);
+		gameBoard [0, 1] = "wH";	Instantiate (whiteHorse, new Vector3 (0, whiteHorse.transform.position.y, 1), Quaternion.identity);
 		gameBoard [0, 2] = "wB";	Instantiate (whiteBishop, new Vector3 (0, whiteBishop.transform.position.y, 2), Quaternion.identity);
 		gameBoard [0, 3] = "wQ";	Instantiate (whiteQueen, new Vector3 (0, whiteQueen.transform.position.y, 3), Quaternion.identity);
 		gameBoard [0, 4] = "wK";	Instantiate (whiteKing, new Vector3 (0, whiteKing.transform.position.y, 4), whiteKing.transform.rotation);
@@ -44,11 +46,25 @@ public class GameController : MonoBehaviour {
 		gameBoard [7, 4] = "bK";	Instantiate (blackKing, new Vector3 (7, blackKing.transform.position.y, 4), blackKing.transform.rotation);
 		gameBoard [7, 5] = "bB";	Instantiate (blackBishop, new Vector3 (7, blackBishop.transform.position.y, 5), Quaternion.identity);
 		gameBoard [7, 6] = "bH";	Instantiate (blackHorse, new Vector3 (7, blackHorse.transform.position.y, 6), Quaternion.identity);
-		gameBoard [7, 7] = "bR";	Instantiate (blackRook, new Vector3 (7, blackRook.transform.position.y, 7), Quaternion.identity);
-	}
+		gameBoard [7, 7] = "bR";	Instantiate (blackRook, new Vector3 (7, blackRook.transform.position.y, 7), Quaternion.identity);*/
 
-
-	public void UpdateGameBoard(){
+		Instantiate (whiteRook, new Vector3 (0, whiteRook.transform.position.y, 0), Quaternion.identity);
+		Instantiate (whiteHorse, new Vector3 (0, whiteHorse.transform.position.y, 1), Quaternion.identity);
+		Instantiate (whiteBishop, new Vector3 (0, whiteBishop.transform.position.y, 2), Quaternion.identity);
+		Instantiate (whiteQueen, new Vector3 (0, whiteQueen.transform.position.y, 3), Quaternion.identity);
+		Instantiate (whiteKing, new Vector3 (0, whiteKing.transform.position.y, 4), whiteKing.transform.rotation);
+		Instantiate (whiteBishop, new Vector3 (0, whiteBishop.transform.position.y, 5), Quaternion.identity);
+		Instantiate (whiteHorse, new Vector3 (0, whiteHorse.transform.position.y, 6), Quaternion.identity);
+		Instantiate (whiteRook, new Vector3 (0, whiteRook.transform.position.y, 7), Quaternion.identity);
+		
+		Instantiate (blackRook, new Vector3 (7, blackRook.transform.position.y, 0), Quaternion.identity);
+		Instantiate (blackHorse, new Vector3 (7, blackHorse.transform.position.y, 1), Quaternion.identity);
+		Instantiate (blackBishop, new Vector3 (7, blackBishop.transform.position.y, 2), Quaternion.identity);
+		Instantiate (blackQueen, new Vector3 (7, blackQueen.transform.position.y, 3), Quaternion.identity);
+		Instantiate (blackKing, new Vector3 (7, blackKing.transform.position.y, 4), blackKing.transform.rotation);
+		Instantiate (blackBishop, new Vector3 (7, blackBishop.transform.position.y, 5), Quaternion.identity);
+		Instantiate (blackHorse, new Vector3 (7, blackHorse.transform.position.y, 6), Quaternion.identity);
+		Instantiate (blackRook, new Vector3 (7, blackRook.transform.position.y, 7), Quaternion.identity);
 
 	}
 
@@ -74,17 +90,53 @@ public class GameController : MonoBehaviour {
 			case "White":
 				WhitePiecesController whitePieceController = selectedPiece.GetComponent<WhitePiecesController> ();
 				if(whitePieceController.IsMoveValid(coordToMove)){
-					selectedPiece.transform.position = coordToMove;
-					Debug.Log ("Animation_moving"); //Play animation(moving)
-					selectedPiece = null;
+					switch(gameBoard[(int)coordToMove.x, (int)coordToMove.z]){
+						case 0:
+							gameBoard[(int)selectedPiece.transform.position.x, (int)selectedPiece.transform.position.z] = 0;
+							gameBoard[(int)coordToMove.x, (int)coordToMove.z] = 1;
+							selectedPiece.transform.position = coordToMove;
+							Debug.Log ("Animation_moving"); //Play animation(moving)
+							selectedPiece = null;
+							break;
+
+						case 1:
+							break;
+
+						case 2:
+							gameBoard[(int)selectedPiece.transform.position.x, (int)selectedPiece.transform.position.z] = 0;
+							gameBoard[(int)coordToMove.x, (int)coordToMove.z] = 1;
+							selectedPiece.transform.position = coordToMove;
+							Debug.Log ("Animation_moving"); //Play animation(moving)
+							Debug.Log ("Animation_eating"); //Play animation(eating)
+							selectedPiece = null;
+							break;
+					}
 				}
 				break;
 			case "Black":	
 				BlackPiecesController blackPieceController = selectedPiece.GetComponent<BlackPiecesController> ();
 				if(blackPieceController.IsMoveValid(coordToMove)){
-					selectedPiece.transform.position = coordToMove;
-					Debug.Log ("Animation_moving"); //Play animation(moving)
-					selectedPiece = null;
+					switch(gameBoard[(int)coordToMove.x, (int)coordToMove.z]){
+						case 0:
+							gameBoard[(int)selectedPiece.transform.position.x, (int)selectedPiece.transform.position.z] = 0;
+							gameBoard[(int)coordToMove.x, (int)coordToMove.z] = 2;
+							selectedPiece.transform.position = coordToMove;
+							Debug.Log ("Animation_moving"); //Play animation(moving)
+							selectedPiece = null;
+							break;
+
+						case 1:
+							gameBoard[(int)selectedPiece.transform.position.x, (int)selectedPiece.transform.position.z] = 0;
+							gameBoard[(int)coordToMove.x, (int)coordToMove.z] = 2;
+							selectedPiece.transform.position = coordToMove;
+							Debug.Log ("Animation_moving"); //Play animation(moving)
+							Debug.Log ("Animation_eating"); //Play animation(eating)
+							selectedPiece = null;
+							break;
+
+						case 2:
+							break;
+					}
 				}
 				break;
 		}
